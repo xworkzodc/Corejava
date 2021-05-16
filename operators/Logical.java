d18087d224cf5f54b8f34d31b2dcc8d531887c7d
sachingm96 <83223198+sachingm96@users.noreply.github.com>
2021-05-16 23:20:10 +0530

    Add files via upload

---
+++ operators/Logical.java
@@ -0,0 +1,13 @@
+class Logical {
+public static void main (String[] args) {
+int a= 5;
+int b=5;
+
+
+System.out.println((a==6) && (++b==6));
+System.out.println(b);
+System.out.println((a==6) & (++b==6));
+System.out.println(b);
+
+}
+}
---
+++ operators/Relational.java
@@ -0,0 +1,16 @@
+class Relational {
+public static void main (String[] args) {
+int a= 10;
+int b=6;
+
+
+
+
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a==b);
+System.out.println(a!=b);
+
+
+}
+}
