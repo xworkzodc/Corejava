4e5dc5f8a714c815e513dfb97d1bbcecbeac6d51
sachingm96 <83223198+sachingm96@users.noreply.github.com>
2021-05-19 17:11:38 +0530

    Delete operators directory

--- operators/Ternary.java
+++
@@ -1,8 +0,0 @@
-class Ternary {
-public static void main (String[] args) {
-int marks=40;
-String result;
-result= (marks >=35)? "pass" : "fail";
-System.out.println("you " + result+" the exam");
-}
-}
