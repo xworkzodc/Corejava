9b0ce8b4117e74292732a4d3c9581343cf91e1b2
sachingm96 <sachingm06@gmail.com>
2021-07-17 20:21:35 +0530

    operators example

---
+++ PrecedenceAndAssociativity.java
@@ -0,0 +1,13 @@
+class PrecedenceAndAssociativity{
+public static void main (String[] args) {
+int a= 10;
+int b=20;
+int c=30;
+
+
+System.out.println(++a>b && c>b);
+System.out.println(a+++b);
+
+
+}
+}
